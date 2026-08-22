d0ae8cb2bebb5686518b05db9f13a014766a4762
AbbasiDaniel <danielabbasi2010@gmail.com>
2026-08-22 16:06:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,38 @@
 from flask import Flask, request, render_template, flash
 import numpy as np
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import time, re, threading, sqlite3 , random , math , os
+import os
 from flask import redirect, url_for, jsonify
-import requests, json 
 import random, cv2
+import AI_emotions, Calculations, Prompt
 
-#print(cv2.__version__)
-#print(hasattr(cv2, 'CascadeClassifier'))
-#########################
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("msambare/fer2013")
-
-print("Path to dataset files:", path)
-
-
-####################
 app = Flask(__name__, template_folder='.')
 app.secret_key = 'javidshahjavidshahjavidshah'
+
+    
+def Responds(emotions_list, intensity, stability, wavelength, burstiness):
+    return Prompt.give_advice(emotions_list, intensity, stability, wavelength, burstiness)
+
+def calcualtions(Emotions):
+    features=[]
+    features.append(Calculations.calculate_intensity(Emotions))
+    features.append(Calculations.calculate_stability(Emotions))
+    features.append(Calculations.calculate_wavelength(Emotions))
+    features.append(Calculations.calculate_burstiness(Emotions))
+    return features
+    
+def extract_emotions(faces):
+    emotions=[]
+    for face in faces:
+        #print("*")
+        emotions.append(AI_emotions.tell_emotion(face))
+    return np.array(emotions)
+
 
 def extract_faces(pixels):
     cascade_file = 'haarcascade_frontalface_default.xml'
     
     if not os.path.exists(cascade_file):
-        return jsonify({'status': 'error', 'message': 'فایل مدل تشخیص چهره در پوشه پروژه یافت نشد!'}), 500
+        return jsonify({'status': 'error', 'message': 'there is not file!'}), 500
         
     face_cascade = cv2.CascadeClassifier(cascade_file)
     cropped_faces = []
@@ -38,8 +45,10 @@ def extract_faces(pixels):
             x, y, w, h = faces[0]
             face = frame[y:y+h, x:x+w]
             face_resized = cv2.resize(face, (224, 224))
+            #print("+")
             cropped_faces.append(face_resized)
         else:
+            #print("+")
             cropped_faces.append(cv2.resize(frame, (224, 224)))
 
     return np.array(cropped_faces)
@@ -48,7 +57,7 @@ def extract_faces(pixels):
 def pixelization():
     video_path = os.path.join('uploads', 'user_video.webm')
     cap = cv2.VideoCapture(video_path)
-    fps = int(cap.get(cv2.CAP_PROP_FPS)) or 30
+    fps = 30
     
     pixels = []
     frame_count = 0
@@ -56,43 +65,66 @@ def pixelization():
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
+            #print(f"Video ended early at frame: {frame_count}")
             break
             
         if frame_count % fps == 0:
+            #print("=")
             pixels.append(frame)
             
         frame_count += 1
 
     cap.release()
     return np.array(pixels)
+
 def SaveVideo(video):
     os.makedirs('uploads', exist_ok=True)
     save_path = os.path.join('uploads', 'user_video.webm')
     video.save(save_path)
+
 @app.route("/", methods=["GET", "POST"])   
 def index():
     if request.method == "POST":
-        print("shaloom")
+        
         video = request.files.get('video')
-
         if not video:
-            return jsonify({'status': 'error', 'message': 'ridim haji!'}), 400
-
+            return jsonify({'status': 'error', 'message': 'there is no file!'}), 400
+        
         SaveVideo(video)
-        pixs=pixelization()
-        pixs=extract_faces(pixs)
-        print(pixs.shape)
+        pixs = pixelization()
+        pixs = extract_faces(pixs)
+        emotions = extract_emotions(pixs)
+        features = calcualtions(emotions)
+
+        intensity = float(features[0])
+        stability = float(features[1])
+        wavelength = float(features[2])
+        burstiness = float(features[3])
+        
+        responds = Responds(emotions, intensity, stability, wavelength, burstiness)
+        explain = responds[0]
+        advice = responds[1]
+
+        if isinstance(emotions, np.ndarray):
+            emotions_list = emotions.tolist()
+        else:
+            emotions_list = list(emotions)
+            
+        emotions_str = ", ".join(emotions_list)
+
         cv2.imwrite('test_face.jpg', pixs[0])
+        
         return jsonify({
             'status': 'success',
-            'ai_engine': {
-                'analysis': 'Calm & High Focus Detected',
-                'advice': 'Signal processing completed. Facial frame matrix successfully generated.'
-            }
+            'explain': str(explain),
+            'advice': str(advice),
+            'emotions_str': emotions_str,
+            'intensity': intensity,
+            'stability': stability,
+            'wavelength': wavelength,
+            'burstiness': burstiness
         })
-        #return redirect(url_for('index'))
+    
     return render_template("templates/index.html")
-
-
 if __name__ == "__main__":
     app.run(debug=True,use_reloader=False)
